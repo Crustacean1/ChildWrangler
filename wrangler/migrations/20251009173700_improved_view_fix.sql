49a695bcc77d8735930862e923f1f87de7e93b48
Kamil Kowalski <kamilkowalski314@gmail.com>
2025-10-09 21:20:57 +0200

    fix: typo

--- wrangler/migrations/20251009173700_improved_view_fix.sql
+++ wrangler/migrations/20251009173700_improved_view_fix.sql
@@ -3,5 +3,5 @@ DROP VIEW total_attendance;
 CREATE VIEW total_attendance AS SELECT DISTINCT ON (day, meal_id, students.id) effective_attendance.value, effective_attendance.day, effective_attendance.meal_id, students.id AS student_id, effective_attendance.cause_id FROM students
 						INNER JOIN group_relations ON group_relations.child = students.id
 						INNER JOIN effective_attendance ON effective_attendance.target = group_relations.parent
-						WHERE student.removed = false
+						WHERE students.removed = false
 ORDER BY day, meal_id, students.id, value, level;
