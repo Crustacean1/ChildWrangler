ffef7982ff4abaac719d8bf56855b482d7c44e0b
Kamil Kowalski <kamilkowalski314@gmail.com>
2025-10-21 21:32:07 +0200

    fix: improved build experience

--- Containerfile
+++ Containerfile
@@ -2,7 +2,7 @@ FROM --platform=$BUILDPLATFORM ubuntu:noble AS builder
 ARG TARGETPLATFORM
 ARG BUILDPLATFORM
 
-RUN apt-get update && apt-get install -y curl build-essential
+RUN apt-get update && apt-get install -y curl build-essential gcc-arm-linux-gnueabihf gcc-aarch64-linux-gnu libc6-dev:arm64
 
 ENV PATH="$PATH:/root/.cargo/bin" 
 
@@ -12,10 +12,15 @@ COPY ./wrangler ./
 COPY ./build.sh ./
 COPY ./setup.sh ./
 
+RUN rustup target add aarch64-unknown-linux-gnu
+RUN rustup target add armv7-unknown-linux-gnueabihf 
+
+ENV CARGO_TARGET_DIR=/output
+ENV CARGO_TARGET_AARCH64_UNKNOWN_LINUX_GNU_LINKER=/usr/bin/aarch64-linux-gnu-gcc
+ENV CARGO_TARGET_ARMV7_UNKNOWN_LINUX_GNUEABIHF_LINKER=/usr/bin/arm-linux-gnueabihf-gcc
 
 RUN ./setup.sh
 RUN cargo test
-ENV CARGO_TARGET_DIR=/output
 RUN ./build.sh
 
 FROM --platform=$TARGETPLATFORM gcr.io/distroless/cc-debian12 AS child_wrangler
@@ -23,7 +28,7 @@ FROM --platform=$TARGETPLATFORM gcr.io/distroless/cc-debian12 AS child_wrangler
 WORKDIR /wrangler
 
 COPY --from=builder /output/release/child_wrangler /wrangler/
-COPY --from=builder /output  /wrangler/target/site
+COPY --from=builder /wrangler/target/site  /wrangler/target/site
 
 ENTRYPOINT ["/wrangler/child_wrangler"]
 
--- build.sh
+++ build.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
 
 echo "Target: ${TARGETPLATFORM}"
-
 		
 case $TARGETPLATFORM in
 	linux/amd64)
-		echo "Building for x86 linux"
-		cargo leptos build --release	
-		cargo leptos build --bin message_daemon	
+		echo "x86 linux detected"
 	;;
 	linux/arm64)
-		rustup target add aarch64-unknown-linux-gnu
-		LEPTOS_BIN_TARGET_TRIPLE="aarch64-unknown-linux-gnu" cargo leptos build --release	
-		LEPTOS_BIN_TARGET_TRIPLE="aarch64-unknown-linux-gnu" cargo leptos build --bin message_daemon	
-		echo "Building for  aarch linux"
+		echo "AARCH 64 linux detected"
+		export LEPTOS_BIN_TARGET_TRIPLE="aarch64-unknown-linux-gnu" 
 	;;
 	linux/arm/v7)
-		echo "Builidng for ARMv7 linux"
-
-		apt-get install -yqq gcc-arm-linux-gnueabihf
-		echo "[target.armv7-unknown-linux-gnueabihf]\nlinker = \"arm-linux-gnueabihf-gcc\"" >> ~/.cargo/config.toml
-		rustup target add armv7-unknown-linux-gnueabihf 
-		LEPTOS_BIN_TARGET_TRIPLE="armv7-unknown-linux-gnueabihf" cargo leptos build --release	
-		LEPTOS_BIN_TARGET_TRIPLE="armv7-unknown-linux-gnueabihf" cargo leptos build --bin message_daemon	
+		echo "ARMv7 linux detected"
+		export LEPTOS_BIN_TARGET_TRIPLE="armv7-unknown-linux-gnueabihf"
 	;;
 	*)
 		echo "Target not recognized, quitting"
+		exit 1
 	;;
 esac
+
+cargo leptos build --release	
+cargo leptos build --bin message_daemon	
