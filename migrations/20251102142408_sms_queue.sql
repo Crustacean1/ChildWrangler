fe747ac819752e0cd7e8375165a5e996d4ac5308
Kamil Kowalski <kamilkowalski314@gmail.com>
2025-11-02 16:18:24 +0100

    wip

---
+++ migrations/20251102142408_sms_queue.sql
@@ -0,0 +1,25 @@
+DROP TABLE messages;
+ 
+CREATE TABLE messages (
+	id uuid primary key default gen_random_uuid(),
+	content text NOT NULL ,
+	phone text NOT NULL,
+	processed bool NOT NULL default false,
+	outgoing bool NOT NULL,
+	inserted timestamp default LOCALTIMESTAMP(0),
+	sent timestamp
+);
+
+CREATE OR REPLACE FUNCTION notifymsg() RETURNS trigger AS $$
+DECLARE
+BEGIN
+  IF NEW.outgoing THEN
+    PERFORM pg_notify('sent');
+  ELSE  
+    PERFORM pg_notify('received');
+  END IF;
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER notify_msg AFTER INSERT ON messages FOR EACH ROW EXECUTE FUNCTION notifymsg();
